58eca66fe40db916c48eb4604159a316e0818aa0
Shagun Khemka <shagun.khemka60@gmail.com>
2019-10-21 01:28:41 +0530

    Add swagger generated files

--- Trello_quora/quora-api/target/generated-sources/io/swagger/api/ApiException.java
+++ Trello_quora/quora-api/target/generated-sources/io/swagger/api/ApiException.java
@@ -1,6 +1,6 @@
 package io.swagger.api;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-10-19T17:13:40.782+05:30")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-10-20T23:54:58.621+05:30")
 
 public class ApiException extends Exception{
     private int code;
--- Trello_quora/quora-api/target/generated-sources/io/swagger/configuration/SwaggerDocumentationConfig.java
+++ Trello_quora/quora-api/target/generated-sources/io/swagger/configuration/SwaggerDocumentationConfig.java
@@ -10,7 +10,7 @@ import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-10-19T17:13:40.782+05:30")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-10-20T23:54:58.621+05:30")
 
 @Configuration
 public class SwaggerDocumentationConfig {
