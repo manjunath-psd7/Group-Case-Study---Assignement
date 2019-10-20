dc46a7f407afda9c4c3d585e92fc0df3d4a83b91
Shagun Khemka <shagun.khemka60@gmail.com>
2019-10-21 01:33:38 +0530

    Add QuestionEntity file for db interaction

--- Trello_quora/quora-service/src/main/java/com/upgrad/quora/service/entity/QuestionEntity.java
+++ Trello_quora/quora-service/src/main/java/com/upgrad/quora/service/entity/QuestionEntity.java
@@ -1,4 +1,78 @@
 package com.upgrad.quora.service.entity;
 
-public class QuestionEntity {
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "QUESTION", schema = "public")
+@NamedQueries(
+        {
+                @NamedQuery(name = "questionById", query = "select q from QuestionEntity q where q.uuid = :uuid"),
+                @NamedQuery(name = "questionByUserId", query = "select q from QuestionEntity q where q.user.uuid = :user_id")
+        }
+)
+public class QuestionEntity implements Serializable {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "UUID")
+    @Size(max = 200)
+    private String uuid;
+
+    @Column(name = "QUESTION")
+    @NotNull
+    @Size(max = 200)
+    private String question;
+
+    @Column(name = "DATE")
+    private ZonedDateTime createdDate;
+
+    @ManyToOne
+    @JoinColumn(name = "USER_ID")
+    private UserEntity user;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public ZonedDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(ZonedDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public UserEntity getUser() {
+        return user;
+    }
+
+    public void setUser(UserEntity user) {
+        this.user = user;
+    }
 }
